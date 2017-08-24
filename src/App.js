d1a128758a6e5136a5d4df6b072a91baa6e4228e
Jonathan Stevens <jstevens@covermymeds.com>
2017-08-24 12:53:30 -0400

    Fixed word add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      wordList: []
+      wordList: [],
+      newWordValue: ''
     }
     this.addWord = this.addWord.bind(this);
   }
 
-  addWord = (event) => {
-    this.setState({ wordList.push(event.target.value) })
+  addWord = (e) => {
+    debugger
+    e.preventDefault();
+    this.setState({wordList: this.state.wordList.concat(this.state.newWordValue)})
+  }
+
+  captureNewWordValue = (e) => {
+    this.setState({newWordValue: e.target.value});
   }
 
   render() {
     return (
       <div>
         <form onSubmit={this.addWord}>
-          <input ></input>
+          <input onChange={this.captureNewWordValue}></input>
           <button>Add word</button>
         </form>
       </div>
