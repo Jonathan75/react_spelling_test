1d4dc485c09ccfb80864b93fb2324c65ec26a074
Jonathan Stevens <jstevens@covermymeds.com>
2018-06-08 09:22:35 -0400

    Spaces not tabs

--- src/components/quizBuilder.js
+++ src/components/quizBuilder.js
@@ -3,19 +3,19 @@ import WordList from './wordList'
 import speak from '../lib/speak'
 
 class QuizBuilder extends Component {
-	render(){
-		return(
-			<div>
-				<form onSubmit={this.props.addWord}>
-					<input type="text" id="word" />
-					<button>Add word</button>
-				</form>
-				<WordList wordList={this.props.wordList} />
-				<button onClick={this.props.startQuiz}>Start Test</button>
-				<button onClick={() => speak('Testing 1,2,3')}>Audio Test</button>
-			</div>
-		)
-	}
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.props.addWord}>
+          <input type="text" id="word" />
+          <button>Add word</button>
+        </form>
+        <WordList wordList={this.props.wordList} />
+        <button onClick={this.props.startQuiz}>Start Test</button>
+        <button onClick={() => speak('Testing 1,2,3')}>Audio Test</button>
+      </div>
+    )
+  }
 
 }
 
--- src/lib/speak.js
+++ src/lib/speak.js
@@ -1,13 +1,13 @@
 const speak = (text) => {
-	console.log('speaking', text)
-	const u = new SpeechSynthesisUtterance()
-	u.text = text
-	u.lang = 'en-US'
-	// u.voice = voice
-	u.rate = 1.0
-	u.pitch = 1.0
-	u.volume = 1
-	window.speechSynthesis.speak(u)
+  console.log('speaking', text)
+  const u = new SpeechSynthesisUtterance()
+  u.text = text
+  u.lang = 'en-US'
+  // u.voice = voice
+  u.rate = 1.0
+  u.pitch = 1.0
+  u.volume = 1
+  window.speechSynthesis.speak(u)
 }
 
 export default speak
