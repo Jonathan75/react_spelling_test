a4c699c5fef5b657e1e8fa4e9e6a1f280998bc64
Jonathan Stevens <jstevens@covermymeds.com>
2018-06-27 13:02:07 -0400

    Word List test

--- jest/App.test.js
+++ jest/App.test.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from '../src/App'
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+}
+global.localStorage = localStorageMock
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
