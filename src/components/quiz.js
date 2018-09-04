a87726651376ffccbcb7839367b02dc19fd7e765
Jonathan Stevens <jstevens@covermymeds.com>
2018-09-04 13:37:25 -0400

    Set state

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react'
 
 class Quiz extends Component {
-  constructor(props) {
-  super(props);
-  //this.state = { counter: 0 };
-  //this.handleClick = this.handleClick.bind(this);
-  this.state = { peek: false}
-  }
+  // constructor(props) {
+  //   super(props)
+  //   //this.handleClick = this.handleClick.bind(this);
+  //   this.state = {peek: false}
+  // }
+
+  state = {peek: false}
 
   componentDidMount(){
      this.nameInput.focus()
   }
 
-  peekShow = () => {
-    this.setState(peek: true)
+  peekShow = (e) => {
+    e.preventDefault()
+    // debugger
+    console.log(this.state)
+    this.setState({peek: true})
   }
 
   onSubmitWord = (e) => {
     e.preventDefault()
-    this.nameInput.focus();
+    this.nameInput.focus()
     if (this.props.evaluateWord(e.target.elements["word"].value)){
       e.target.elements["word"].value = ''
     }
@@ -32,8 +36,10 @@ class Quiz extends Component {
             <input type='text' id="word" ref={(input) => { this.nameInput = input }}></input>
           </div>
           <div>
-            <button onClick={this.peek}>Peek</button>
-            <div style={this.state.showMyComponent ? {} : { display: 'none' }}>{this.props.currentWord}</div>
+            <button onClick={this.peekShow}>Peek</button>
+            <div style={this.state.peek ? {} : { display: 'none' }}>{this.props.currentWord}</div>
+            <div>peek is</div>
+            <div>{`${this.state.peek} and ${this.props.currentWord}`}</div>
           </div>
           <div>
             <button>Continue</button>
